6448489e471075330f99032751a09740d3ee73ef
Shilo B <shiloworks@gmail.com>
2019-06-05 09:55:51 +1000

    Simplified default portal settings. Added feature to suppress types. Upgraded libraries with security alerts. Added version variables into the build script.

--- deploySnapshotToBranch.sh
+++ deploySnapshotToBranch.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+NVM_VERSION=v0.34.0
+NODE_VERSION=10.16.0
+NG_VERSION=8.0.1
+YARN_VERSION=1.5.1
 
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
@@ -7,8 +11,8 @@ setup_git() {
 
 build() {
   apt-get update; apt-get install wget git -y
-  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-  export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; nvm install 8.11.0; nvm use 8.11.0; npm install -g @angular/cli yarn@1.5.1; yarn install; ng build --prod --build-optimizer --output-path build/peppermint
+  wget -qO- https://raw.githubusercontent.com/creationix/nvm/$NVM_VERSION/install.sh | bash
+  export NG_CLI_ANALYTICS=ci; export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; nvm install $NODE_VERSION; nvm use $NODE_VERSION; npm install -g @angular/cli@$NG_VERSION yarn@$YARN_VERSION; yarn install; ng build --prod --build-optimizer --output-path build/peppermint
 }
 
 commit_files() {
